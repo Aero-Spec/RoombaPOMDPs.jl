ed34ba42e9a649eeb8c2afbd63c85f6a50dea631
Anshrin Srivastava <Anshrin@gmail.com>
2025-07-21 17:28:52 -0400

    Update filtering.jl

--- src/filtering.jl
+++ src/filtering.jl
@@ -1,23 +1,17 @@
 # specification of particle filters for the bumper and lidar Roomba environments
 # maintained by {jmorton2,kmenda}@stanford.edu
 
+using ParticleFilters: LowVarianceResampler, ParticleCollection, WeightedParticleBelief, resample, initialize_belief
+using Random
+using StaticArrays
+
 """
 Definition of the particle filter for the Roomba environment
-
 Fields:
 - `v_noise_coeff::Float64` coefficient to scale particle-propagation noise in velocity
 - `om_noise_coeff::Float64` coefficient to scale particle-propagation noise in turn-rate
 """
-
-# filtering.jl
-
-import ParticleFilters
-
-const resample_random! = ParticleFilters.resample_random!
-const resample = ParticleFilters.resample
-const WeightedParticleBelief = ParticleFilters.WeightedParticleBelief
-
-mutable struct RoombaParticleFilter{M<:RoombaModel, RM, RNG<:AbstractRNG, PMEM} <: Updater
+mutable struct RoombaParticleFilter{M<:RoombaModel,RM,RNG<:AbstractRNG,PMEM} <: Updater
     model::M
     resampler::RM
     n_init::Int
@@ -28,16 +22,16 @@ mutable struct RoombaParticleFilter{M<:RoombaModel, RM, RNG<:AbstractRNG, PMEM} 
     _weight_memory::Vector{Float64}
 end
 
-function RoombaParticleFilter(model, n, v_noise_coeff, om_noise_coeff;
-                              resampler=resample_random!, rng=Random.GLOBAL_RNG)
+function RoombaParticleFilter(model, n::Integer, v_noise_coeff, om_noise_coeff, resampler=LowVarianceResampler(n), rng::AbstractRNG=Random.GLOBAL_RNG)
     return RoombaParticleFilter(model,
-                                resampler,
-                                n,
-                                v_noise_coeff,
-                                om_noise_coeff,
-                                rng,
-                                Vector{typeof(model)}(undef, n),
-                                Vector{Float64}(undef, n))
+                               resampler,
+                               n,
+                               v_noise_coeff,
+                               om_noise_coeff,
+                               rng,
+                               sizehint!(particle_memory(model), n),
+                               sizehint!(Float64[], n)
+                              )
 end
 
 # Modified Update function adds noise to the actions that propagate particles
@@ -47,12 +41,11 @@ function POMDPs.update(up::RoombaParticleFilter, b::ParticleCollection, a, o)
     empty!(pm)
     empty!(wm)
     all_terminal = true
-    for s in ParticleFilters.particles(b)
+    for s in particles(b)
         if !isterminal(up.model, s)
             all_terminal = false
             # noise added here:
-            a_pert = a + SVec2(up.v_noise_coeff * (rand(up.rng) - 0.5),
-                               up.om_noise_coeff * (rand(up.rng) - 0.5))
+            a_pert = a + SVec2(up.v_noise_coeff * (rand(up.rng) - 0.5), up.om_noise_coeff * (rand(up.rng) - 0.5))
             sp = @gen(:sp)(up.model, s, a_pert, up.rng)
             push!(pm, sp)
             push!(wm, obs_weight(up.model, s, a_pert, sp, o))
@@ -63,14 +56,13 @@ function POMDPs.update(up::RoombaParticleFilter, b::ParticleCollection, a, o)
         error("Particle filter update error: all states in the particle collection were terminal.")
     end
 
-    return ParticleFilters.resample(up.resampler,
-                                    WeightedParticleBelief(pm, wm, sum(wm), nothing),
-                                    up.model,
-                                    up.model,
-                                    b, a, o,
-                                    up.rng)
+    return resample(up.resampler,
+                    WeightedParticleBelief(pm, wm, sum(wm), nothing),
+                    up.model,
+                    up.model,
+                    b, a, o,
+                    up.rng)
 end
 
 # initialize belief state
-ParticleFilters.initialize_belief(up::RoombaParticleFilter, d) =
-    ParticleCollection([rand(up.rng, d) for i in 1:up.n_init])
+initialize_belief(up::RoombaParticleFilter, d) = ParticleCollection([rand(up.rng, d) for _ in 1:up.n_init])
