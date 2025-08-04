f55b1a217a3cb16fa905276c7df92698a6dcac37
Anshrin Srivastava <Anshrin@gmail.com>
2025-08-04 14:34:12 -0400

    Update filtering.jl

--- src/filtering.jl
+++ src/filtering.jl
@@ -1,9 +1,12 @@
-using ParticleFilters: LowVarianceResampler, WeightedParticleBelief
+# specification of particle filters for the bumper and lidar Roomba environments
+# maintained by {jmorton2,kmenda}@stanford.edu
 
-function particle_memory(model)
-    Vector{RoombaState}()
-end
-
+"""
+Definition of the particle filter for the Roomba environment
+Fields:
+- `v_noise_coeff::Float64` coefficient to scale particle-propagation noise in velocity
+- `om_noise_coeff::Float64`coefficient to scale particle-propagation noise in turn-rate
+"""
 mutable struct RoombaParticleFilter{M<:RoombaModel,RM,RNG<:AbstractRNG,PMEM} <: Updater
     model::M
     resampler::RM
@@ -15,57 +18,47 @@ mutable struct RoombaParticleFilter{M<:RoombaModel,RM,RNG<:AbstractRNG,PMEM} <: 
     _weight_memory::Vector{Float64}
 end
 
-function RoombaParticleFilter(
-    model, n::Integer, v_noise_coeff, om_noise_coeff, 
-    resampler=LowVarianceResampler(n), rng::AbstractRNG=Random.GLOBAL_RNG
-)
-    return RoombaParticleFilter(
-        model,
-        resampler,
-        n,
-        v_noise_coeff,
-        om_noise_coeff,
-        rng,
-        sizehint!(particle_memory(model), n),
-        sizehint!(Float64[], n)
-    )
+function RoombaParticleFilter(model, n::Integer, v_noise_coeff, om_noise_coeff, resampler=LowVarianceResampler(n), rng::AbstractRNG=Random.GLOBAL_RNG)
+    return RoombaParticleFilter(model,
+                               resampler,
+                               n,
+                               v_noise_coeff,
+                               om_noise_coeff,
+                               rng,
+                               sizehint!(particle_memory(model), n),
+                               sizehint!(Float64[], n)
+                              )
 end
 
-function POMDPs.update(up::RoombaParticleFilter, b::Vector{RoombaState}, a, o)
+# Modified Update function adds noise to the actions that propagate particles
+function POMDPs.update(up::RoombaParticleFilter, b::ParticleCollection, a, o)
     pm = up._particle_memory
     wm = up._weight_memory
     empty!(pm)
     empty!(wm)
     all_terminal = true
-    for s in b
+    for s in particles(b)
         if !isterminal(up.model, s)
             all_terminal = false
+            # noise added here:
             a_pert = a + SVec2(up.v_noise_coeff * (rand(up.rng) - 0.5), up.om_noise_coeff * (rand(up.rng) - 0.5))
             sp = @gen(:sp)(up.model, s, a_pert, up.rng)
             push!(pm, sp)
             push!(wm, obs_weight(up.model, s, a_pert, sp, o))
         end
     end
+    # if all particles are terminal, issue an error
     if all_terminal
         error("Particle filter update error: all states in the particle collection were terminal.")
     end
 
-    return WeightedParticleBelief(pm, wm)
+    return ParticleFilters.resample(up.resampler,
+                    WeightedParticleBelief(pm, wm, sum(wm), nothing),
+                    up.model,
+                    up.model,
+                    b, a, o,
+                    up.rng)
 end
 
-ParticleFilters.initialize_belief(up::RoombaParticleFilter, d) = [rand(up.rng, d) for i in 1:up.n_init]
-
-# --- POLICY/CONTROL FALLBACKS ---
-
-struct ToEnd end
-
-function POMDPTools.action(p::ToEnd, s::RoombaState)
-    # TODO: Replace with actual logic for your robot
-    return RoombaAct(0.0, 0.0)
-end
-
-function POMDPTools.action(p::ToEnd, b::WeightedParticleBelief{RoombaState})
-    idx = argmax(b.weights)
-    s = b.particles[idx]
-    return POMDPTools.action(p, s)   # <--- THIS LINE IS KEY!
-end
+# initialize belief state
+ParticleFilters.initialize_belief(up::RoombaParticleFilter, d) = ParticleCollection([rand(up.rng, d) for i in 1:up.n_init])
