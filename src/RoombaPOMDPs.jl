7f7d8a1b73f515608fe2082dab7b91396933ddcb
Anshrin Srivastava <Anshrin@gmail.com>
2025-08-08 16:14:43 -0400

    Update RoombaPOMDPs.jl

--- src/RoombaPOMDPs.jl
+++ src/RoombaPOMDPs.jl
@@ -1,8 +1,6 @@
 module RoombaPOMDPs
 
 using POMDPs
-import POMDPs: initialize_belief   # Only if you extend it
-
 using Distributions
 using StaticArrays
 using Parameters
@@ -11,11 +9,10 @@ using Statistics
 using Graphics
 using Cairo
 using Random
-import ParticleFilters
-import POMDPTools: render
-using ParticleFilters: resample
+using ParticleFilters
 
-# Export all main API symbols for users of your package
+import POMDPTools: render
+
 export
     RoombaState,
     RoombaAct,
@@ -35,10 +32,9 @@ export
     DiscreteRoombaStateSpace,
     render
 
-# Include the submodules/files in the right order (so dependencies are loaded before used)
 include("line_segment_utils.jl")
 include("env_room.jl")
 include("roomba_env.jl")
 include("filtering.jl")
 
-end # module RoombaPOMDPs
+end
